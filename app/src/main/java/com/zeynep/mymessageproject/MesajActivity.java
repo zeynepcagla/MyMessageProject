25fa8933bf035b969d2a1353329db339ac69ddb1
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-05-12 22:49:27 +0300

    Mesaj Gönderme İşlemi

--- app/src/main/java/com/zeynep/mymessageproject/MesajActivity.java
+++ app/src/main/java/com/zeynep/mymessageproject/MesajActivity.java
@@ -2,14 +2,18 @@ package com.zeynep.mymessageproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,16 +21,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.zeynep.mymessageproject.Model.User;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MesajActivity extends AppCompatActivity {
 
     private ImageButton geritusu;
-    private TextView    kullaniciadi;
-    private EditText   mesajgirdi;
-    private ImageView  fotoekle,gonder;
+    private TextView kullaniciadi;
+    private EditText mesajgirdi;
+    private ImageView fotoekle, gonder;
     private CircleImageView profilresim;
     private Intent intent;
+    private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,8 @@ public class MesajActivity extends AppCompatActivity {
         geritusu = findViewById(R.id.geritusu);
         kullaniciadi = findViewById(R.id.kullaniciAdiMesaj);
         profilresim = findViewById(R.id.profilresim);
+        mesajgirdi = findViewById(R.id.mesajGirdiAlani);
+
         fotoekle = findViewById(R.id.fotoEkleMesaj);
         gonder = findViewById(R.id.gonderBtn);
 
@@ -43,14 +52,14 @@ public class MesajActivity extends AppCompatActivity {
     }
 
     private void kullaniciBilgisiAl() {
-    intent = getIntent();
-    final String userId = intent.getStringExtra("userId");
+        intent = getIntent();
+        final String userId = intent.getStringExtra("userId");
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user =snapshot.getValue(User.class);
+                User user = snapshot.getValue(User.class);
                 kullaniciadi.setText(user.getName_surname());
             }
 
@@ -59,6 +68,46 @@ public class MesajActivity extends AppCompatActivity {
 
             }
         });
+        gonder.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String mesajgirdisi = mesajgirdi.getText().toString();
+                        if (!mesajgirdisi.equals("")) {
 
-    }
-}
+                            MesajGonderen(firebaseUser.getUid(), userId, mesajgirdisi);
+
+                            MesajAlan(firebaseUser.getUid(), userId, mesajgirdisi);
+
+                        } else {
+                            Toast.makeText(MesajActivity.this, "Mesaj Kısmı Boş", Toast.LENGTH_SHORT).show();
+                        }
+                        mesajgirdi.setText("");
+                    }
+                });
+    }//kullaniciBilgisiAl()
+    private void MesajGonderen(String gonderen, String alici, String mesaj) {
+        HashMap<String, Object> mMap=  new HashMap<>();
+        mMap.put("gonderen",gonderen);
+        mMap.put("alici",alici);
+        mMap.put("mesaj",mesaj);
+        mMap.put("resim", "");
+
+        FirebaseDatabase.getInstance().getReference().child("Mesajlar").child(firebaseUser.getUid()).push().setValue(mMap);
+
+    }//MesajGonder()
+    private void MesajAlan(String gonderen, String alici, String mesaj) {
+        HashMap<String, Object> mMap=  new HashMap<>();
+        mMap.put("gonderen",gonderen);
+        mMap.put("alici",alici);
+        mMap.put("mesaj",mesaj);
+        mMap.put("resim", "");
+
+        FirebaseDatabase.getInstance().getReference().child("Mesajlar").child(alici).push().setValue(mMap);
+
+    }//MesajAlan()
+
+}//end
+
+
+
